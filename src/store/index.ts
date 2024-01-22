4205b1ab7928c737e19905278974757120997382
menyaboo <tosha.kolmay@gmail.com>
2024-01-23 06:12:19 +0700

    add reducer animeApi

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
+import {animeApi} from "../services/animeApi";
 
 export const store = configureStore({
   reducer: {
-
+    [animeApi.reducerPath]: animeApi.reducer,
   },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware({
-      serializableCheck: false,
-    })
-  },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(animeApi.middleware),
+});
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
