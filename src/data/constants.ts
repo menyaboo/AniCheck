0795f6d7dfb9e754c704f3f62d3c2a330b10481d
menyaboo <tosha.kolmay@gmail.com>
2024-01-23 06:11:02 +0700

    add constants.ts

---
+++ src/data/constants.ts
@@ -0,0 +1,5 @@
+const STORAGE_IMAGE_URL = 'https://static-libria.weekstorm.one'
+const BASE_API_URL = "https://api.anilibria.tv/v3/"
+const API_PARAMS = "?filter=id,names[ru],posters[medium][url],player[alternative_player],description"
+
+export {STORAGE_IMAGE_URL, BASE_API_URL, API_PARAMS}
