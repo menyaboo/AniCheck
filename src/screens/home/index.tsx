f4263c6249842844b6dccc87494584da324d4834
menyaboo <tosha.kolmay@gmail.com>
2024-01-23 06:14:12 +0700

    feat: home-page

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,7 +1,8 @@
-import {ScreenView} from "../../styles/global";
+import {ScreenView} from '../../styles/global';
+import {TitleList} from "../../components/home-screen/title-list";
 
 export const Home = () => (
   <ScreenView>
-
+    <TitleList />
   </ScreenView>
 )
