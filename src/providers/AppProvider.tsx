85be0f298d25ba348d460338714f04091e112220
menyaboo <tosha.kolmay@gmail.com>
2024-01-25 15:32:19 +0700

    add screen and types

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,12 +1,8 @@
-import {Provider} from "react-redux";
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
 import {FC, PropsWithChildren} from "react";
+import {Provider} from "react-redux";
 
 import {store} from "../store";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {StatusBar} from "expo-status-bar";
-
-const Stack = createNativeStackNavigator();
 
 export const AppProvider: FC<PropsWithChildren> = ({children}) => (
   <Provider store={store}>
