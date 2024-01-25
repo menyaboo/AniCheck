7ea6df9eafb5aa347d7bef37632d94ead1ee05b2
menyaboo <tosha.kolmay@gmail.com>
2024-01-25 15:33:54 +0700

    feat: title-screen

--- src/screens/home-screen/index.tsx
+++ src/screens/home-screen/index.tsx
@@ -1,7 +1,7 @@
 import {ScreenView} from '../../styles/global';
 import {TitleList} from "../../components/home-screen/title-list";
 
-export const Home = () => (
+export const HomeScreen = () => (
   <ScreenView>
     <TitleList />
   </ScreenView>
